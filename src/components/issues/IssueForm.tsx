0a0b121901edef2f87f26f52e2454bfebd6cf46a
FawadSoomro877 <fawadsoomro877@gmail.com>
2024-02-08 16:39:18 +0500

    fix caching issues

--- src/components/issues/IssueForm.tsx
+++ src/components/issues/IssueForm.tsx
@@ -43,6 +43,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
         await axios.patch("/api/issues/" + issue.id, data);
       } else await axios.post("/api/issues", data);
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setIsSubmitting(false);
       setError("An error occurred. Try correct values");
